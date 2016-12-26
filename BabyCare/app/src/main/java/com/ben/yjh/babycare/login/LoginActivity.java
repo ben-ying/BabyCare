e710c99675e8ea5eed093b2f59f3c34660c8ffc4
Ben <benying1988@gmail.com>
2016-12-26 17:02:15 +0800

    valid input #4

--- BabyCare/app/src/main/java/com/ben/yjh/babycare/login/LoginActivity.java
+++ BabyCare/app/src/main/java/com/ben/yjh/babycare/login/LoginActivity.java
@@ -4,17 +4,21 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
 import com.ben.yjh.babycare.R;
 import com.ben.yjh.babycare.base.BaseActivity;
+import com.ben.yjh.babycare.util.AlertUtils;
 import com.ben.yjh.babycare.util.Constants;
 
 public class LoginActivity extends BaseActivity {
 
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
+    private String mUsername;
+    private String mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,39 @@ public class LoginActivity extends BaseActivity {
         mPasswordEditText = (EditText) findViewById(R.id.et_password);
     }
 
+    private boolean isValid() {
+        mUsername = mUsernameEditText.getText().toString().trim();
+        mPassword = mPasswordEditText.getText().toString().trim();
+
+        if (mUsername.isEmpty()) {
+            AlertUtils.showAlertDialog(this, R.string.empty_username);
+            return false;
+        }
+        if (mPassword.isEmpty()) {
+            AlertUtils.showAlertDialog(this, R.string.empty_password);
+            return false;
+        }
+        if (mPassword.length() < 6) {
+            AlertUtils.showAlertDialog(this, R.string.invalid_password);
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(mUsername).matches()) {
+            AlertUtils.showAlertDialog(this, R.string.invalid_email);
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
     public void onClick(View v) {
         Intent intent;
 
         switch (v.getId()) {
             case R.id.btn_login:
+                if (isValid()) {
+                    // todo login
+                }
                 break;
             case R.id.tv_link_signup:
                 intent = new Intent(LoginActivity.this, SignUpActivity.class);
--- BabyCare/app/src/main/java/com/ben/yjh/babycare/login/SignUpActivity.java
+++ BabyCare/app/src/main/java/com/ben/yjh/babycare/login/SignUpActivity.java
@@ -1,21 +1,70 @@
 package com.ben.yjh.babycare.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 
 import com.ben.yjh.babycare.R;
 import com.ben.yjh.babycare.base.BaseActivity;
+import com.ben.yjh.babycare.util.AlertUtils;
 
 public class SignUpActivity extends BaseActivity {
+
+    private EditText mUsernameEditText;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mConfirmPasswordEditText;
+    private String mUsername;
+    private String mEmail;
+    private String mPassword;
+    private String mConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mUsernameEditText = (EditText) findViewById(R.id.et_username);
+        mEmailEditText = (EditText) findViewById(R.id.et_email);
+        mPasswordEditText = (EditText) findViewById(R.id.et_password);
+        mConfirmPasswordEditText = (EditText) findViewById(R.id.et_confirm_password);
         findViewById(R.id.tv_link_login).setOnClickListener(this);
         findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+
+    private boolean isValid() {
+        mUsername = mUsernameEditText.getText().toString().trim();
+        mEmail = mEmailEditText.getText().toString().trim();
+        mPassword = mPasswordEditText.getText().toString().trim();
+        mConfirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        if (mUsername.isEmpty()) {
+            AlertUtils.showAlertDialog(this, R.string.empty_username);
+            return false;
+        }
+        if (mEmail.isEmpty()) {
+            AlertUtils.showAlertDialog(this, R.string.empty_email);
+            return false;
+        }
+        if (mPassword.isEmpty()) {
+            AlertUtils.showAlertDialog(this, R.string.empty_password);
+            return false;
+        }
+        if (mPassword.length() < 6) {
+            AlertUtils.showAlertDialog(this, R.string.invalid_password);
+            return false;
+        }
+        if (!mPassword.equals(mConfirmPassword)) {
+            AlertUtils.showAlertDialog(this, R.string.invalid_confirm_password);
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(mUsername).matches()) {
+            AlertUtils.showAlertDialog(this, R.string.invalid_email);
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -25,6 +74,9 @@ public class SignUpActivity extends BaseActivity {
                 onBackPressed();
                 break;
             case R.id.btn_register:
+                if (isValid()) {
+                    // todo register
+                }
                 break;
         }
     }
