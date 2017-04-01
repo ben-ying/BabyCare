3ae35755ab492bd013e43a10aef10099bf3a2762
Ben <benying1988@gmail.com>
2017-04-01 17:02:19 +0800

    fix bugs

--- BabyCare/app/src/main/java/com/ben/yjh/babycare/main/ImagePagerActivity.java
+++ BabyCare/app/src/main/java/com/ben/yjh/babycare/main/ImagePagerActivity.java
@@ -1,6 +1,7 @@
 package com.ben.yjh.babycare.main;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
@@ -24,7 +25,7 @@ public class ImagePagerActivity extends BaseActivity {
     private List<String> mUrls;
     private String mCurrentUrl;
     private View mRootView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
+    private final Runnable mShowStatusBarRunnable = new Runnable() {
         @Override
         public void run() {
             ActionBar actionBar = getSupportActionBar();
@@ -33,15 +34,16 @@ public class ImagePagerActivity extends BaseActivity {
             }
         }
     };
-    private final Runnable mHidePart2Runnable = new Runnable() {
+    private final Runnable mHideStatusBarRunnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            mRootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            mRootView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
 //                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
@@ -50,9 +52,9 @@ public class ImagePagerActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_pager);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         mRootView = findViewById(R.id.content_layout);
-        mRootView.setOnClickListener(this);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -64,7 +66,6 @@ public class ImagePagerActivity extends BaseActivity {
         }
         ImageViewpagerAdapter pagerAdapter = new ImageViewpagerAdapter(this, mUrls);
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        mViewPager.setOnClickListener(this);
         mViewPager.setOffscreenPageLimit(2);
         mViewPager.setAdapter(pagerAdapter);
         mViewPager.setCurrentItem(mUrls.indexOf(mCurrentUrl));
@@ -81,16 +82,15 @@ public class ImagePagerActivity extends BaseActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+        mHideHandler.removeCallbacks(mShowStatusBarRunnable);
+        mHideHandler.postDelayed(mHideStatusBarRunnable, UI_ANIMATION_DELAY);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.content_layout:
-                finish();
-                break;
+
+
         }
     }
 }
