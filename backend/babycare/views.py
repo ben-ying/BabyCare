6bf98acf8dfb9185364a3d8f9541ad69450084f5
Ben <benying1988@gmail.com>
2017-04-07 15:30:35 +0800

    add views #19

---
+++ backend/babycare/views.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import pdb
+
+from rest_framework.decorators import api_view
+from babycare.models import Feedback
+from constants import CODE_SUCCESS, FEEDBACK_FOOTER_IMAGE
+from constants import MSG_SEND_FEEDBACK_SUCCESS
+from utils import invalid_token_response, get_user_by_token, save_error_log, upload_image_to_oss
+from utils import simple_json_response
+from django.utils import timezone
+
+
+@api_view(['POST'])
+def send_feedback(request):
+    description = request.data.get('description')
+    base64_images = request.data.get('base64_images')
+    token = request.data.get('token')
+    user = get_user_by_token(token)
+
+    try:
+        if user:
+            feedback = Feedback()
+            feedback.user = user
+            feedback.description = description
+            feedback.created = timezone.now()
+            i = 0
+            if base64_images:
+                for image in base64_images:
+                    i += 1
+                    image_name = user.username + time.strftime('%Y%m%d%H%M%S') + FEEDBACK_FOOTER_IMAGE
+                    image = upload_image_to_oss(image_name, image)
+                    if i == 1:
+                        feedback.image1 = image
+                    if i == 2:
+                        feedback.image2 = image
+                    if i == 3:
+                        feedback.image3 = image
+                    if i == 4:
+                        feedback.image4 = image
+                    if i == 5:
+                        feedback.image5 = image
+                    if i == 6:
+                        feedback.image6 = image
+                    if i == 7:
+                        feedback.image7 = image
+                    if i == 8:
+                        feedback.image8 = image
+                    if i == 9:
+                        feedback.image9 = image
+            feedback.save()
+            return simple_json_response(CODE_SUCCESS, MSG_SEND_FEEDBACK_SUCCESS)
+        else:
+            return invalid_token_response()
+    except Exception as e:
+        return save_error_log(request, e)
