33f778d4a7cb930aad7d4cde20816e6f2d335194
ben <jiahi.ying@preh.cn>
2017-10-24 11:28:23 +0800

    remove backend

--- BabyCare/app/src/main/java/com/ben/yjh/babycare/glide/MyGlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.ben.yjh.babycare.glide;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class MyGlideModule extends AppGlideModule {}
--- BabyCare/app/src/main/java/com/ben/yjh/babycare/widget/recyclerview/LoadMoreListener.java
+++
@@ -1,5 +0,0 @@
-package com.ben.yjh.babycare.widget.recyclerview;
-
-public interface LoadMoreListener {
-    void onLoadMore();
-}
